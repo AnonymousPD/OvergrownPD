56824727863ab6ffe8d15f4cb69e33615087abdc
TypedScroll <leondanielalexanderhorn@gmail.com>
2019-07-10 03:33:17 +0200

    v0.0.3: Updated copyright

--- core/src/main/java/com/overgrownpixel/overgrownpixeldungeon/BackupHandler.java
+++ core/src/main/java/com/overgrownpixel/overgrownpixeldungeon/BackupHandler.java
@@ -4,7 +4,8 @@
  *
  * Shattered Pixel Dungeon
  * Copyright (C) 2014-2019 Evan Debenham
- *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2019 Evan Debenham
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
--- core/src/main/java/com/overgrownpixel/overgrownpixeldungeon/effects/particles/SacrificialParticle.java
+++ core/src/main/java/com/overgrownpixel/overgrownpixeldungeon/effects/particles/SacrificialParticle.java
@@ -5,6 +5,9 @@
  * Shattered Pixel Dungeon
  * Copyright (C) 2014-2019 Evan Debenham
  *
+ * Overgrown Pixel Dungeon
+ * Copyright (C) 2018-2019 Anon
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
